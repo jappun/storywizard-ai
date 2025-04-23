f71bbf41160fc320566e8cac30f581edf43a44f2
jappun <jappun@student.ubc.ca>
2025-04-22 23:02:29 -0700

    frontend boilerplate

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Frame } from "./components/Frame"
+
+function App() {
+
+  return (
+      <div className="bg-amber-100 w-screen h-screen">
+        <Frame />
+      </div>
+  )
+}
+
+export default App
