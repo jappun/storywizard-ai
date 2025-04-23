4e96bc1e28ef995c2a0ebeb23914103992f2308e
jappun <jappun@student.ubc.ca>
2025-04-22 21:41:28 -0700

    merged some questions

--- story.py
+++ story.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
-elements = {"place": "", "place_details": "", "name": "", "species": "", "likes":"", "dislikes":"", "looks":"", "personality":"", "conflict":"", "rising":"", "climax":"", "falling":"", "resolution":""}
+elements = {"place": "", "place_details": "", "name": "", "species": "", "looks":"", "details":"", "conflict":"", "rising":"", "climax":"", "falling":"", "resolution":""}
 
 class PlotElement:
     def __init__(self, prompt, element):
@@ -15,24 +15,17 @@ class PlotElement:
         self.element = element
 
 
-# TODO: limit these
-# place -
-# main character
-# - name
-# - merge likes/dislikes/personality into fun facts
 element_questions = [
     PlotElement("place? ", "place"),
     PlotElement("place details?  ", "place_details"),
     PlotElement("p name?  ", "name"),
     PlotElement("p species? ", "species"),
-    PlotElement("p likes? ", "likes"),
-    PlotElement("p dislikes? ", "dislikes"),
     PlotElement("p looks? ", "looks"),
-    PlotElement("p personality? ", "personality"),
+    PlotElement("p details? ", "details"),
     PlotElement("conflict? ", "conflict"),
     PlotElement("rising? ",  "rising"),
     PlotElement("climax? ", "climax"),
-    PlotElement("falling? ", "falling"),
+    PlotElement("falling? ", "falling"), 
     PlotElement("resolution? ", "resolution")
 ]
 
@@ -41,14 +34,14 @@ def build_story(element_questions):
     for pe in element_questions:
         elements[pe.element] = input(pe.prompt)
 
-    prompt = "Write a 500-word short story in the style of a third grader. " \
+    prompt = "Write a 500-word short story in the style of a fifth grader. " \
             "Come up with a title for it. " \
             "Ensure you use a basic and easy to follow plot structure. " \
             "Below are details on key plot elements you must use. " \
             "If any of the details are illogical or grammatically incorrect, make the minimal possible corrections in your story. " \
             f"Setting: This story takes place in {elements['place']}. {elements['place']} looks like {elements['place_details']}. " \
-            f"Protagonist: This story is about {elements['name']}, who is a {elements['species']}. {elements['name']} likes {elements['likes']} and dislikes {elements['dislikes']}. " \
-            f"They look like {elements['looks']} and their personality is like {elements['personality']}. " \
+            f"Protagonist: This story is about {elements['name']}, who is a {elements['species']}. " \
+            f"They look like {elements['looks']} and these are some extra details about then: {elements['details']}. " \
             f"Conflict: The main problem our protagonist faces is: {elements['conflict']}. " \
             f"Rising action(s): Before solving their conflict, our protagonist tries many things and faces many obstacles. These are: {elements['rising']}. " \
             f"Climax: In the most emotionally intense part of the story, the hero solves their main conflict. They do this by {elements['climax']}. " \
